b8931be521f36f96790367eee24dc68475ee2ade
MariFer14 <ferarenilla@gmail.com>
2024-04-10 16:56:32 -0500

    feat/Ejercicio 4 colores

---
+++ assets/js/4Colores.js
@@ -0,0 +1,20 @@
+const figAzul = document.getElementById("figAzul");
+const figRojo = document.getElementById("figRojo");
+const figVerde = document.getElementById("figVerde");
+const figAmarillo = document.getElementById("figAmarillo");
+
+figAzul.addEventListener("click", function () {
+  figAzul.style.backgroundColor = "black";
+});
+
+figRojo.addEventListener("click", function () {
+    figRojo.style.backgroundColor = "black";
+});
+
+figVerde.addEventListener("click", function () {
+    figVerde.style.backgroundColor = "black";
+});
+
+figAmarillo.addEventListener("click", function () {
+    figAmarillo.style.backgroundColor = "black";
+});
