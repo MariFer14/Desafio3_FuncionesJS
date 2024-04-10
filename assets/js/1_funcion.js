b892aeb9ecea1412d230d9d58bf1478cf31eb0dc
MariFer14 <ferarenilla@gmail.com>
2024-04-10 14:01:41 -0500

    Primer ejercicio JS realizado

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,10 @@
+/* FUNCIÓN DADA */
+// function example(a, b, c) {
+//   return a + b + c;
+// }
+
+/* FUNCIÓN MODIFICADA */
+example = function (a, b, c) {
+  resultado = a + b + c;
+  alert(resultado);
+};
